df7abfca65a323b12f1135765f87d6dcc3df1ff7
golembio <golembjovskijm@gmail.com>
2026-05-01 17:57:42 +0200

    feat: added PaymentMapper, and Request/Respond dto

--- backend/src/main/java/com/akademikplus/akademik_plus/entity/Payment.java
+++ backend/src/main/java/com/akademikplus/akademik_plus/entity/Payment.java
@@ -18,7 +18,7 @@ import java.time.LocalDate;
 public class Payment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+    private Long id;
 
     @ManyToOne
     @JoinColumn(name = "user_id", nullable = false)
