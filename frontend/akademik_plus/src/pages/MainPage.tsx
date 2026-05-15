62600aea6292f3bf708278be3790cf17c454c34a
B1ona4y <eu.oleynich@gmail.com>
2026-05-15 19:31:05 +0200

    Frontend: Finale setup. Base imports and structure.

---
+++ frontend/akademik_plus/src/pages/MainPage.tsx
@@ -0,0 +1,12 @@
+import Topbar from '../components/Topbar'
+
+export default function MainPage() {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <Topbar />
+      <main>
+        
+      </main>
+    </div>
+  )
+}
