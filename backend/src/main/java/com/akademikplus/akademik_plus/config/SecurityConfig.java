14f23f03d86413ea7c4b12e6aa1b6ed085afe5dd
golembio <golembjovskijm@gmail.com>
2026-06-07 23:08:43 +0200

    fix: added permissions and cors conmfiguration.

--- backend/src/main/java/com/akademikplus/akademik_plus/config/SecurityConfig.java
+++ backend/src/main/java/com/akademikplus/akademik_plus/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import com.akademikplus.akademik_plus.security.UserDetailServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -16,6 +17,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -28,9 +34,25 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http.csrf(csrf -> csrf.disable())
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
 
                 .authorizeHttpRequests(auth -> auth
+                        //admin
                         .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers("/api/users/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.POST, "/api/rooms/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.PUT,"/api/rooms/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.DELETE,"/api/rooms/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.DELETE,"/api/payments/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.DELETE,"/api/maintenance-requests/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.PATCH,"/api/maintenance-requests/**").hasRole("ADMIN")
+
+                        //admin and student
+                        .requestMatchers(HttpMethod.GET,"/api/rooms/**").hasAnyRole("ADMIN", "STUDENT")
+                        .requestMatchers(HttpMethod.GET,"/api/payments/**").hasAnyRole("ADMIN", "STUDENT")
+                        .requestMatchers(HttpMethod.POST,"/api/payments/**").hasAnyRole("ADMIN", "STUDENT")
+                        .requestMatchers(HttpMethod.GET,"/api/maintenance-requests/**").hasAnyRole("ADMIN", "STUDENT")
+                        .requestMatchers(HttpMethod.POST,"/api/maintenance-requests/**").hasAnyRole("ADMIN", "STUDENT")
                         .anyRequest().authenticated()
                 )
 
@@ -60,5 +82,19 @@ public class SecurityConfig {
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
         return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
