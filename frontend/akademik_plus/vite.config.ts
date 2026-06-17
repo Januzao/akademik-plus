3b2f7f4e122db376fd42aa47f18b5fb66cd32fce
B1ona4y <eu.oleynich@gmail.com>
2026-06-17 18:19:34 +0200

    feat: docker compose update

--- frontend/akademik_plus/vite.config.ts
+++ frontend/akademik_plus/vite.config.ts
@@ -3,12 +3,15 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
+const backendTarget = process.env.VITE_PROXY_TARGET ?? "http://localhost:8080"
+
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
+    host: true,
     proxy: {
-      "/api": "http://localhost:8080",
-      "/uploads": "http://localhost:8080"
-    }
-  }
+      "/api": backendTarget,
+      "/uploads": backendTarget,
+    },
+  },
 })
